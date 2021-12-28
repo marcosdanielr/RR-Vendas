617e265b0d0a3a10f2ecae1cff49f397b5721617
unknown <marcosdaniel99@protonmail.com>
2021-12-28 18:06:26 -0300

    RR Vendas

---
+++ js/CamisasFemininas.js
@@ -0,0 +1,32 @@
+const items = [
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: './img/test1.png',    
+        quantidade: 0
+        
+    },
+
+    {
+        id: 1,
+        nome: 'camiseta2',
+        img: './img/test1.png',
+        quantidade: 0
+    },
+
+    {
+        id: 2,
+        nome: 'camiseta3',
+        img: './img/test1.png',
+        quantidade: 0
+    },
+
+    {
+        id: 3,
+        nome: 'camiseta4',
+        img: './img/test1.png',
+        quantidade: 0
+    },
+    
+]
+
---
+++ js/carrinho.js
@@ -0,0 +1,58 @@
+
+inicializarLoja=() =>
+{
+    var containerProdutos = document.getElementById('produtos');
+    items.map((val)=>{
+        containerProdutos.innerHTML+= `
+        
+        <div class="produto-single">
+            <img src="`+val.img+`" />
+            <p>`+val.nome+`</p>
+            <a key="`+val.id+`"href="#">Adicionar ao carrinho!</a>
+            </div>
+
+        `;
+    })
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () =>        
+{
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    items.map((val)=>{
+        if(val.quantidade >0){
+        containerCarrinho.innerHTML+= `
+        <div class="info-single-checkout">
+        <p style="float:left;">Produto: `+val.nome+`</p>
+        <p style="float:right;">Quantidade: `+val.quantidade+`</p>
+        <div style="clear:both"></div>
+        <hr>
+        </div>
+
+        `;
+        }
+    })
+
+}
+
+
+
+var links = document.getElementsByTagName('a');
+
+for(var i = 0; i < links.length; i++){
+    links[i].addEventListener("click",function(){
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho();
+        return false;               
+
+    })
+}
+
+
+useEffect(() =>{    
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}, [carrinho])
+
